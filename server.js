449136f32f3e3347c76f9320065c7776ac37e5d0
Nick Phelps <Nick90052@Gmail.com>
2018-12-13 16:15:47 -0600

    First Three

--- server.js
+++ server.js
@@ -37,12 +37,44 @@ var owners = [
 
 
 // GET /api/owners
+app.get('/api/owners', function(req,res, nextFn) {
+    console.log(owners)
+    res.json(owners)
+})
 
 // GET /api/owners/:id
+app.get('/api/owners/:id', function(req, res, nextFn) {
+    let myArray = []
+    const myID = req.params.id
+    console.log('Entered URL ID...',myID)
+    for (let i = 0; i < owners.length; i++) {
+        if (owners[i].id = myID) {
+            myArray = owners[i]
+        }
+    }
+    console.log(myArray)
+    res.json(myArray)
+})
 
 // POST /api/owners
+app.post('/api/owners', function(req, res, nextFn) {
+    myAddedObject = req.body
+    owners.push(myAddedObject)
+    console.log(owners)
+})
 
 // PUT /api/owners/:id
+app.put('/api/owners/:id', function(req, res, nextFn) {
+    const myID = req.params.id
+    const updatedText = req.body.name //going to change the id
+
+    for (let i = 0; i < owners.length; i++) {
+        if (owners[i].id == myID) {
+            owners[i].name = updatedText
+        }
+    }
+    console.log(owners)
+})
 
 // DELETE /api/owners/:id
 
